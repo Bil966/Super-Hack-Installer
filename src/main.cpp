ade3cde3e2584bd1c3c029c18a0ea7647297864e
NeonGD1667 <leanbinht@gmail.com>
2026-06-21 19:32:43 +0700

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,68 +1,8 @@
-
 #include <Geode/Geode.hpp>
 #include <Geode/modify/MenuLayer.hpp>
-#include <Geode/ui/Popup.hpp>
+#include "ui/MainPopup.hpp"
 
 using namespace geode::prelude;
-
-// =====================================
-// POPUP
-// =====================================
-
-class SuperHackPopup : public geode::Popup {
-protected:
-    bool init() {
-        if (!Popup::init(360.f, 220.f))
-            return false;
-
-        this->setTitle("SUPER HACK INSTALLER");
-
-        auto label = CCLabelBMFont::create(
-            "Super Hack Menu Ready To Install",
-            "goldFont.fnt"
-        );
-
-        label->setScale(0.45f);
-        label->setPosition(
-            m_size.width / 2,
-            m_size.height / 2 + 10.f
-        );
-
-        m_mainLayer->addChild(label);
-
-        auto footer = CCLabelBMFont::create(
-            "Made by Homeless Team & NeonGD",
-            "chatFont.fnt"
-        );
-
-        footer->setScale(0.45f);
-        footer->setPosition(
-            m_size.width / 2,
-            20.f
-        );
-
-        m_mainLayer->addChild(footer);
-
-        return true;
-    }
-
-public:
-    static SuperHackPopup* create() {
-        auto ret = new SuperHackPopup();
-
-        if (ret && ret->init()) {
-            ret->autorelease();
-            return ret;
-        }
-
-        delete ret;
-        return nullptr;
-    }
-};
-
-// =====================================
-// MENU BUTTON
-// =====================================
 
 class $modify(MyMenuLayer, MenuLayer) {
     bool init() {
@@ -100,7 +40,6 @@ class $modify(MyMenuLayer, MenuLayer) {
     }
 
     void onSuperHack(CCObject*) {
-        SuperHackPopup::create()->show();
+        MainPopup::create()->show();
     }
 };
-
